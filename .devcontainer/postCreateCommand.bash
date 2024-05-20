ad7ba900d656127310dc796a68ec0b6c7b5c6380
ion098 <146852218+ion098@users.noreply.github.com>
2024-05-20 21:35:52 +0000

    fixed copy and paste error in postCreateCommand

--- .devcontainer/postCreateCommand.bash
+++ .devcontainer/postCreateCommand.bash
@@ -4,6 +4,6 @@ pushd $PWD
 cd ~
 curl https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2 -L -O
 tar -xjvf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
-echo \"export PATH=$PATH:/home/codespace/gcc-arm-none-eabi-10.3-2021.10/bin\" >> ~/.bashrc
+echo "PATH=$PATH:/home/codespace/gcc-arm-none-eabi-10.3-2021.10/bin" >> ~/.bashrc
 source ~/.bashrc
 popd
