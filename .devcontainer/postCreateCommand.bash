adf2ac905f81a0af5911e8569b47fa7804c1527c
ion098 <146852218+ion098@users.noreply.github.com>
2024-05-20 21:54:41 +0000

    changed postCreateCommand.bash, i hate github codespaces

--- .devcontainer/postCreateCommand.bash
+++ .devcontainer/postCreateCommand.bash
@@ -5,7 +5,5 @@ cd ~
 curl https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2 -L -O
 tar -xjvf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 echo "PATH=$PATH:/home/codespace/gcc-arm-none-eabi-10.3-2021.10/bin" >> ~/.bashrc
-source ~/.bashrc
 popd
 source ~/.bashrc
-exec bash
