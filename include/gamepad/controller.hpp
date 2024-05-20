2b3a9c77f7974323fbb95179410012ad32fa80cf
ion098 <146852218+ion098@users.noreply.github.com>
2024-05-20 20:30:43 +0000

    Fixed some compile issues, refactored some stuff, added devcontainer.json

--- include/gamepad/controller.hpp
+++ include/gamepad/controller.hpp
@@ -13,12 +13,30 @@
 #include "pros/rtos.hpp"
 
 namespace Gamepad {
-struct ButtonState {
+
+enum EventType {
+    ON_PRESS,
+    ON_LONG_PRESS,
+    ON_RELEASE,
+};
+
+class Button {
+    public:
     bool rising_edge = false;
     bool falling_edge = false;
     bool is_pressed = false;
     uint32_t last_press_time = pros::millis();
     uint32_t last_release_time = last_press_time;
+
+    uint32_t onPress(std::function<void(void)> func);
+    uint32_t onLongPress(std::function<void(void)> func);
+    uint32_t onRelease(std::function<void(void)> func);
+    uint32_t addListener(EventType event, std::function<void(void)> func);
+    bool removeListener(uint32_t id);
+    private:
+    EventHandler<void> onPressEvent;
+    EventHandler<void> onLongPressEvent;
+    EventHandler<void> onReleaseEvent;
 };
 
 class Controller {
@@ -35,39 +53,19 @@ class Controller {
          * Get the state of a button on the controller.
          * @param button Which button's state you want.
          */
-        const ButtonState& operator[](pros::controller_digital_e_t button);
+        const Button& operator[](pros::controller_digital_e_t button);
         /**
          * Get the value of a joystick axis on the controller.
          * @param joystick Which joystick axis's value to return
          */
         float operator[](pros::controller_analog_e_t joystick);
-        enum EventType {
-            ON_CLICK,
-            ON_RELEASE,
-            ON_LONG_CLICK
-        };
-        /**
-         * Register a function to be run when a specified event occurs on a button.
-         * @param button The button to monitor
-         * @param event The type of event
-         * @param func The function to register
-         * @returns An id number that can be passed to removeHandler
-         */
-        uint32_t addHandler(pros::controller_digital_e_t button, EventType event, std::function<void(void)> func);
-        /**
-         * Un-register a function that was registered with addHandler
-         * @param id The id of the handler to be removed
-         * @returns Whether the handler was removed successfully
-         */
-        bool removeHandler(uint32_t id);
-        ButtonState L1{}, L2{}, R1{}, R2{}, 
+        Button L1{}, L2{}, R1{}, R2{}, 
         Up{}, Down{}, Left{}, Right{}, 
         X{}, B{}, Y{}, A{};
         float LeftX = 0, LeftY = 0, RightX = 0, RightY = 0;
     private:
-        static ButtonState Controller::* button_to_ptr(pros::controller_digital_e_t button);
-        void updateButton(ButtonState Controller::* button, bool is_held);
+        static Button Controller::* button_to_ptr(pros::controller_digital_e_t button);
+        void updateButton(pros::controller_digital_e_t button_id);
         pros::Controller controller;
-        std::map<std::pair<ButtonState Controller::*, EventType>, EventHandler<void>> button_events;
 }; // namespace Gamepad
 }
--- src/gamepad/controller.cpp
+++ src/gamepad/controller.cpp
@@ -1,31 +1,24 @@
 #include "gamepad/controller.hpp"
 
 namespace Gamepad {
-void Controller::updateButton(ButtonState Controller::* button, bool is_held) {
-    this->*button.rising_edge = !this->*button.is_pressed && is_held;
-    this->*button.falling_edge = this->*button.is_pressed && !is_held;
-    this->*button.is_pressed = is_held;
-    if (this->*button.rising_edge) {
-        this->*button.last_press_time = pros::millis();
+void Controller::updateButton(pros::controller_digital_e_t button_id) {
+    Button Controller::* button = Controller::button_to_ptr(button_id);
+    bool is_held = this->controller.get_digital(button_id);
+    (this->*button).rising_edge = !(this->*button).is_pressed && is_held;
+    (this->*button).falling_edge = (this->*button).is_pressed && !is_held;
+    (this->*button).is_pressed = is_held;
+    if ((this->*button).rising_edge) {
+        (this->*button).last_press_time = pros::millis();
     } 
-    if (this->*button.falling_edge) {
-        this->*button.last_release_time = pros::millis();
+    if ((this->*button).falling_edge) {
+        (this->*button).last_release_time = pros::millis();
     }
 }
 
 void Controller::update() {
-    this->updateButton(&Controller::L1);
-    this->updateButton(&Controller::L2);
-    this->updateButton(&Controller::R1);
-    this->updateButton(&Controller::R2);
-    this->updateButton(&Controller::Up);
-    this->updateButton(&Controller::Down);
-    this->updateButton(&Controller::Left);
-    this->updateButton(&Controller::Right);
-    this->updateButton(&Controller::X);
-    this->updateButton(&Controller::B);
-    this->updateButton(&Controller::Y);
-    this->updateButton(&Controller::A);
+    for(auto i = DIGITAL_L1; i != DIGITAL_A; ++i) {
+        this->updateButton(i);
+    }
 
     this->LeftX = this->controller.get_analog(ANALOG_LEFT_X);
     this->LeftY = this->controller.get_analog(ANALOG_LEFT_Y);
@@ -33,22 +26,8 @@ void Controller::update() {
     this->RightY = this->controller.get_analog(ANALOG_RIGHT_Y);
 }
 
-const ButtonState& Controller::operator[](pros::controller_digital_e_t button) {
-    switch (button) {
-        case DIGITAL_L1: return this->L1;
-        case DIGITAL_L2: return this->L2;
-        case DIGITAL_R1: return this->R1;
-        case DIGITAL_R2: return this->R2;
-        case DIGITAL_UP: return this->Up;
-        case DIGITAL_DOWN: return this->Down;
-        case DIGITAL_LEFT: return this->Left;
-        case DIGITAL_RIGHT: return this->Right;
-        case DIGITAL_X: return this->X;
-        case DIGITAL_B: return this->B;
-        case DIGITAL_Y: return this->Y;
-        case DIGITAL_A: return this->A;
-        default: std::exit(1); // TODO: change handling
-    }
+const Button& Controller::operator[](pros::controller_digital_e_t button) {
+    return this->*Controller::button_to_ptr(button);
 }
 
 float Controller::operator[](pros::controller_analog_e_t axis) {
@@ -57,6 +36,24 @@ float Controller::operator[](pros::controller_analog_e_t axis) {
         case ANALOG_LEFT_Y: return this->LeftY;
         case ANALOG_RIGHT_X: return this->RightX;
         case ANALOG_RIGHT_Y: return this->RightY;
+        default: std::exit(1); // TODO: change handling
+    }
+}
+
+Button Controller::* Controller::button_to_ptr(pros::controller_digital_e_t button) {
+    switch (button) {
+        case DIGITAL_L1: return &Controller::L1;
+        case DIGITAL_L2: return &Controller::L2;
+        case DIGITAL_R1: return &Controller::R1;
+        case DIGITAL_R2: return &Controller::R2;
+        case DIGITAL_UP: return &Controller::Up;
+        case DIGITAL_DOWN: return &Controller::Down;
+        case DIGITAL_LEFT: return &Controller::Left;
+        case DIGITAL_RIGHT: return &Controller::Right;
+        case DIGITAL_X: return &Controller::X;
+        case DIGITAL_B: return &Controller::B;
+        case DIGITAL_Y: return &Controller::Y;
+        case DIGITAL_A: return &Controller::A;
         default: std::exit(1); // TODO: change handling
     }
 }
