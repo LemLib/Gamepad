eabc9190d20a2a0f0ebdb2bbc3499095f79e0358
ion098 <146852218+ion098@users.noreply.github.com>
2024-05-23 12:44:07 -0700

    Update postCreateCommand.bash

--- .devcontainer/postCreateCommand.bash
+++ .devcontainer/postCreateCommand.bash
@@ -6,4 +6,3 @@ curl https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/10.3
 tar -xjvf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 echo "PATH=$PATH:/home/codespace/gcc-arm-none-eabi-10.3-2021.10/bin" >> ~/.bashrc
 popd
-source ~/.bashrc
