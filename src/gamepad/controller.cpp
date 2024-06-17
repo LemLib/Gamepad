1ca81b01eb5896eb519a442e71cad99b83c825fb
ion098 <146852218+ion098@users.noreply.github.com>
2024-06-16 19:24:44 -0700

    feat: :sparkles: Add joystick curves to controller
    Allow users to assign a DriveCurve per axis to be used by the controller when updating joystick values.

--- src/gamepad/controller.cpp
+++ src/gamepad/controller.cpp
@@ -3,9 +3,7 @@
 
 namespace Gamepad {
 
-uint32_t Button::onPress(std::function<void(void)> func) {
-    return this->onPressEvent.add_listener(std::move(func));
-}
+uint32_t Button::onPress(std::function<void(void)> func) { return this->onPressEvent.add_listener(std::move(func)); }
 
 uint32_t Button::onLongPress(std::function<void(void)> func) {
     return this->onLongPressEvent.add_listener(std::move(func));
@@ -26,12 +24,8 @@ void Button::update(const bool is_held) {
     } else {
         this->time_released += pros::millis() - last_update_time;
     }
-    if (this->rising_edge) {
-        this->time_held = 0;
-    } 
-    if (this->falling_edge) {
-        this->time_released = 0;
-    }
+    if (this->rising_edge) { this->time_held = 0; }
+    if (this->falling_edge) { this->time_released = 0; }
 
     if (this->rising_edge) {
         onPressEvent.fire();
@@ -43,20 +37,22 @@ void Button::update(const bool is_held) {
 }
 
 void Controller::updateButton(pros::controller_digital_e_t button_id) {
-    Button Controller::* button = Controller::button_to_ptr(button_id);
+    Button Controller::*button = Controller::button_to_ptr(button_id);
     bool is_held = this->controller.get_digital(button_id);
     (this->*button).update(is_held);
 }
 
 void Controller::update() {
-    for(int i = DIGITAL_L1; i != DIGITAL_A; ++i) {
-        this->updateButton(static_cast<pros::controller_digital_e_t>(i));
-    }
+    for (int i = DIGITAL_L1; i != DIGITAL_A; ++i) { this->updateButton(static_cast<pros::controller_digital_e_t>(i)); }
 
     this->LeftX = this->controller.get_analog(ANALOG_LEFT_X);
+    if (this->CurveLeftX) this->LeftX = this->CurveLeftX->curve(this->LeftX);
     this->LeftY = this->controller.get_analog(ANALOG_LEFT_Y);
+    if (this->CurveLeftY) this->LeftY = this->CurveLeftY->curve(this->LeftY);
     this->RightX = this->controller.get_analog(ANALOG_RIGHT_X);
+    if (this->CurveRightX) this->RightX = this->CurveRightX->curve(this->RightX);
     this->RightY = this->controller.get_analog(ANALOG_RIGHT_Y);
+    if (this->CurveRightY) this->RightY = this->CurveRightY->curve(this->RightY);
 }
 
 const Button& Controller::operator[](pros::controller_digital_e_t button) {
@@ -68,13 +64,26 @@ float Controller::operator[](pros::controller_analog_e_t axis) {
         case ANALOG_LEFT_X: return this->LeftX;
         case ANALOG_LEFT_Y: return this->LeftY;
         case ANALOG_RIGHT_X: return this->RightX;
-        case ANALOG_RIGHT_Y: return this->RightY;
-        TODO("change handling for default")
+        case ANALOG_RIGHT_Y: return this->RightY; TODO("change handling for default")
         default: std::exit(1);
     }
 }
 
-Button Controller::* Controller::button_to_ptr(pros::controller_digital_e_t button) {
+void Controller::setCurve(pros::controller_analog_e_t joystick, std::shared_ptr<DriveCurve> curve) {
+    switch (axis) {
+        case ANALOG_LEFT_X: this->CurveLeftX = curve;
+        case ANALOG_LEFT_Y: this->CurveLeftY = curve;
+        case ANALOG_RIGHT_X: this->CurveRightX = curve;
+        case ANALOG_RIGHT_Y: this->CurveRightY = curve; TODO("add error logging")
+        default: errno = EINVAL;
+    }
+}
+
+void Controller::setCurve(pros::controller_analog_e_t joystick, DriveCurve& curve) {
+    this->setCurve(joystick, std::shared_ptr<DriveCurve>(&curve, [](void*) {}));
+}
+
+Button Controller::*Controller::button_to_ptr(pros::controller_digital_e_t button) {
     switch (button) {
         case DIGITAL_L1: return &Controller::L1;
         case DIGITAL_L2: return &Controller::L2;
@@ -87,9 +96,8 @@ Button Controller::* Controller::button_to_ptr(pros::controller_digital_e_t butt
         case DIGITAL_X: return &Controller::X;
         case DIGITAL_B: return &Controller::B;
         case DIGITAL_Y: return &Controller::Y;
-        case DIGITAL_A: return &Controller::A;
-        TODO("change handling for default")
+        case DIGITAL_A: return &Controller::A; TODO("change handling for default")
         default: std::exit(1);
     }
 }
-}
+} // namespace Gamepad
