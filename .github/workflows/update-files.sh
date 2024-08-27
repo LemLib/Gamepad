cdc4acb6ccdfc556cb6e7ba73df9d5e15487ec05
ion098 <146852218+ion098@users.noreply.github.com>
2024-08-27 21:52:23 +0000

    ci: :bug: Fix auto commit in update-files.sh

--- .github/workflows/update-files.sh
+++ .github/workflows/update-files.sh
@@ -2,7 +2,9 @@
 
 # git remote add lemlib https://github.com/LemLib/LemLib.git
 git fetch lemlib
-while IFS= read -r file_name commit_msg; do
+while IFS= read -r file_name; read -r commit_msg; do
     git checkout lemlib/stable -- $file_name
-    git commit -a -m "$commit_msg"
+    if IFS= git status -s | grep "^[MTADRCU]" | read -d '' -n 1; then
+        git commit --message="$commit_msg"
+    fi
 done < .github/workflows/update-files-list.txt
