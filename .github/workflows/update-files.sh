97eb961067f37064366474d591ca93009aa2b338
ion098 <146852218+ion098@users.noreply.github.com>
2024-08-27 20:16:25 +0000

    ci: :bug: Fix incorrect path in update-files.sh

--- .github/workflows/update-files.sh
+++ .github/workflows/update-files.sh
@@ -4,5 +4,5 @@
 git fetch lemlib
 while IFS= read -r file_name commit_msg; do
     git checkout lemlib/stable -- $file_name
-    git -a -m "$commit_msg"
-done < update-files-list.txt
+    git commit -a -m "$commit_msg"
+done < .github/workflows/update-files-list.txt
