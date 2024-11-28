1c07cd36ff742283cbf420ba17e5f4f575690a23
ion098 <146852218+ion098@users.noreply.github.com>
2024-11-28 14:28:02 -0800

    fix: :bug: Fix bugs in Deadband, ExpoCurve, and Transformation
    (Note: Fisheye is still broken)

--- src/gamepad/joystick_transformation.cpp
+++ src/gamepad/joystick_transformation.cpp
@@ -12,8 +12,8 @@ std::pair<float, float> Deadband::get_value(std::pair<float, float> value) {
     float y = value.second;
     float x_deadband = m_x_deadband + abs(y) * m_x_spread;
     float y_deadband = m_y_deadband + abs(x) * m_y_spread;
-    float x_scale = 1 - x_deadband;
-    float y_scale = 1 - y_deadband;
+    float x_scale = 1.0 / (1.0 - x_deadband);
+    float y_scale = 1.0 / (1.0 - y_deadband);
     x = copysign(abs(x) < x_deadband ? 0 : (abs(x) - x_deadband) * x_scale, x);
     y = copysign(abs(y) < y_deadband ? 0 : (abs(y) - y_deadband) * y_scale, y);
     return {x, y};
@@ -23,7 +23,7 @@ std::pair<float, float> ExpoCurve::get_value(std::pair<float, float> value) {
     float x = value.first;
     float y = value.second;
     x = copysign(pow(abs(x), m_x_curve), x);
-    x = copysign(pow(abs(y), m_y_curve), y);
+    y = copysign(pow(abs(y), m_y_curve), y);
     return {x, y};
 }
 
@@ -44,8 +44,7 @@ std::pair<float, float> Fisheye::get_value(std::pair<float, float> value) {
 }
 
 std::pair<float, float> Transformation::get_value(std::pair<float, float> value) {
-    std::pair<float, float> ret_value {};
-    return std::accumulate(m_all_transforms.begin(), m_all_transforms.end(), ret_value,
+    return std::accumulate(m_all_transforms.begin(), m_all_transforms.end(), value,
                            [](auto last_val, auto& next_transform) { return next_transform->get_value(last_val); });
 }
 } // namespace gamepad
